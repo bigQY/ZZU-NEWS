e859f1ca81143f09a89d572fa54413b27119c3fb
bigqy <liuguoqing0394@qq.com>
2024-12-25 23:02:29 +0800

    优化 Service Worker 的请求拦截逻辑，排除 POST 请求和特定 URL 模式，增强缓存管理

--- public/service-worker.js
+++ public/service-worker.js
@@ -27,10 +27,13 @@ self.addEventListener('activate', (event) => {
 // 拦截请求并缓存资源
 self.addEventListener('fetch', (event) => {
   console.log('Fetching:', event.request.url)
+
+  // 排除 POST 请求、带有 'api' 的请求和带参数的 GET 请求
   if (
-    event.request.url.startsWith('http') &&
-    !event.request.url.includes('_nuxt') &&
-    !event.request.url.includes('node_modules')
+    event.request.method !== 'POST' &&
+    !event.request.url.includes('/api/') &&
+    !event.request.url.includes('?') &&
+    event.request.url.startsWith('http')
   ) {
     // 只处理正常网页的请求，并排除特定的 URL 模式
     event.respondWith(
@@ -40,10 +43,12 @@ self.addEventListener('fetch', (event) => {
         }
         return fetch(event.request).then((response) => {
           const clonedResponse = response.clone()
+          const contentType = response.headers.get('Content-Type')
           if (
             event.request.url.endsWith('.js') ||
             event.request.url.endsWith('.css') ||
-            event.request.url.match(/\.(png|jpg|jpeg|gif)$/)
+            event.request.url.match(/\.(png|jpg|jpeg|gif|ico)$/) ||
+            (contentType && contentType.includes('text/html'))
           ) {
             caches.open(CACHE_NAME).then((cache) => {
               cache.put(event.request, clonedResponse)
