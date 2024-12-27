2d65de8ae27872c3eba49668f23e2798e259b668
bigqy <liuguoqing0394@qq.com>
2024-12-27 18:00:08 +0800

    优化服务工作者注册逻辑，添加更新检测功能以实现页面自动刷新

--- app.vue
+++ app.vue
@@ -8,7 +8,20 @@
 if (import.meta.client && import.meta.env.MODE !== 'development' && 'serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/service-worker.js', { updateViaCache: 'none' })
-    .then((registration) => {})
+    .then((registration) => {
+      registration.onupdatefound = () => {
+        const installingWorker = registration.installing
+        installingWorker.onstatechange = () => {
+          if (installingWorker.state === 'installed') {
+            if (navigator.serviceWorker.controller) {
+              // 新的 Service Worker 已经安装完成，页面将被重载
+              alert('发现新版本，页面将自动刷新!')
+              window.location.reload()
+            }
+          }
+        }
+      }
+    })
     .catch((error) => {})
 }
 </script>
